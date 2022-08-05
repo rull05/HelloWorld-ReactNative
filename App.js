a8c2acf31b40a64beaf51c4864077872953fde61
rull <70853147+arl03@users.noreply.github.com>
2022-08-05 00:05:39 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello React Native</Text>
+      <StatusBar style="dark" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'rgb(0, 0, 0)',
+    fontSize: 20
+  }
+});
